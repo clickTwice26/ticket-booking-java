72e481ec0c2d2f674825115374cb27f3d7a44f20
clickTwice26 <shagatoc@gmail.com>
2025-07-02 09:31:14 +0600

    Learned many things at now https://youtu.be/fmX84zu-5gs?t=9028

--- src/main/java/org/example/services/UserBookingService.java
+++ src/main/java/org/example/services/UserBookingService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entities.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 //import org.example.entities.*;
@@ -14,10 +15,11 @@ public class UserBookingService {
     private static final String USERS_PATH = "../localDb/users.json";
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public UserBookingService(User user1) {
+    public UserBookingService(User user1) throws IOException {
         this.user = user1;
         File users = new File(USERS_PATH);
-        userList = objectMapper.readValue(
+        userList = objectMapper.readValue
+                (
                 users, new TypeReference<List<User>>() {
                 }
         );
